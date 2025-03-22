1f8cd237e1597d1ceadfa5e0d93e50b22c1d8e7b
ksksaikishore <ksksaikishore238@gmail.com>
2025-03-22 14:45:06 +0530

    Delete application-code/application-code directory

--- application-code/application-code/app-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'YOUR-DATABASE-ENDPOINT.ap-south-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'YourPassword',
-    DB_DATABASE : 'webappdb'
-});
